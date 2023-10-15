e16f0e06bf4ca1257c84f35d175524d156e6bbcb
adnanalemran <adnanalemranontor@gmail.com>
2023-10-16 01:18:01 +0600

    v-01

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Header from "./components/Header";
-import AddProduct from "./components/AddProduct";
-import EditProduct from "./components/EditProduct";
-import Home from "./components/Home";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Header />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-        loader:()=> fetch('http://localhost:5000/coffee')
-      },
-      {
-        path: "addProduct",
-        element: <AddProduct />,
-      },
-      {
-        path: "editProduct",
-        element: <EditProduct />,
-      },
-    ],
+    element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffee')
   },
+  {
+    path: "addCoffee", 
+    element: <AddCoffee></AddCoffee>
+  }, 
+  {
+    path: 'updateCoffee/:id', 
+    element: <UpdateCoffee></UpdateCoffee>, 
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
+  }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
